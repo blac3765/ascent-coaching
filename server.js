998d6bf42e9de62ff30e0fcff1327d3ac70ba812
Seth <seth@360labs.io>
2019-04-20 15:36:51 -0500

    Staging Build.

---
+++ server.js
@@ -0,0 +1,111 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+const port = process.env.PORT || 8000;
+
+app.listen(port);
+console.log('listening at port: %j', port);
+app.use('/', express.static('./client/dist/ascent-coaching'));
+
+mongoose.connect('mongodb://admin:password1@ds139956.mlab.com:39956/ascent-coaching', {useNewUrlParser:true})
+
+const Article = mongoose.model('Article', {
+	date: Date,
+	title: String,
+	body: String
+});
+
+const Customer = mongoose.model('Customer', {
+	name: String,
+	email: String,
+	service: String,
+	experience: [String],
+	message: String,
+	contact: String,
+})
+
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+	service: 'gmail',
+	auth: {
+		user: 'sethtaylorblack@gmail.com',
+		pass: 'ChemEngr01'
+	}
+});
+
+app.post('/api/email', (req,res) => {
+	if(req.body.experience) {
+		var experience = [];
+		req.body.experience.forEach(e => {
+			switch(e) {
+				case '0':
+					experience.push('0-12 months');
+					return;
+				case '1':
+					experience.push(' 1-2 years');
+					return;
+				case '2':
+					experience.push(' 2-3 years');
+					return;
+				case '3':
+					experience.push(' 3-5 years');
+					return;
+				case '4':
+					experience.push(' 5+ years');
+					return;
+			}
+		})
+	}
+	console.log('experience: %j', experience);
+	var text = [
+		'Type: Contact Form Submission',
+		'Name:     ' 				+ req.body.name.toUpperCase(),
+		'Email:    ' 				+ req.body.email.toUpperCase(),
+		'Phone:    ' 				+ req.body.phone,
+		'Service Requested:      ' 	+ req.body.service.toUpperCase(),
+		'Experience Level: ' 		+ experience,
+		'Message:   ' 				+ req.body.message.toUpperCase(),
+		'Best Contact: ' 			+ req.body.contact
+	].join('\n');
+	console.log('text: %j', text);
+	var mailOptions = {
+		from: 'ascent-coaching@gmail.com',
+		to: req.body.email.toString(),
+		subject: 'Email contact form',
+		text: text
+	}
+	var customer = {
+		name: req.body.name,
+		email: req.body.email,
+		service: req.body.service,
+		experience: experience,
+		message: req.body.message,
+		contact: req.body.contact,
+	}
+	saveCustomer(customer);
+	transporter.sendMail(mailOptions, function(error, info){
+		if (error) {
+			console.log(error);
+			res.send(error);
+		} else {
+			console.log('Email sent: ' + info.response);
+			return res.send(info);
+		}
+	});
+});
+
+function saveCustomer(customer) {
+	if(!customer._id) customer = new Customer(customer);
+	return Customer.findByIdAndUpdate(customer._id, customer, {upsert:true, new:true}).exec();
+}
+
+app.get('/api/blogs', (req, res) => {
+	return Article.find().exec().then(articles => {
+		res.json(articles);
+	})
+});
